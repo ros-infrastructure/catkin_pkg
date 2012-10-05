fa382222d18aabb8ab7597a1739c321555627620
Dirk Thomas <dthomas@willowgarage.com>
2012-10-04 23:01:49 -0700

    refactored topological order, fixed issues with mixing name and relative

--- src/catkin_pkg/topological_order.py
+++ src/catkin_pkg/topological_order.py
@@ -1,12 +1,11 @@
-from __future__ import print_function
-
 from .packages import find_packages
 
 
 class _PackageDecorator(object):
 
-    def __init__(self, package):
+    def __init__(self, package, path):
         self.package = package
+        self.path = path
         self.is_metapackage = 'metapackage' in [e.tagname for e in self.package.exports]
         message_generators = [e.content for e in self.package.exports if e.tagname == 'message_generator']
         self.message_generator = message_generators[0] if message_generators else None
@@ -18,32 +17,40 @@ class _PackageDecorator(object):
 
     def calculate_full_depends(self, packages):
         self.full_depends = set([])
-        package_names = packages.keys()
-        for name in [d.name for d in self.build_depends if d.name in package_names]:
-            self.full_depends.add(name)
+        for name in [d.name for d in (self.package.build_depends + self.package.buildtool_depends) if d.name in packages.keys()]:
             packages[name]._add_recursive_run_depends(packages, self.full_depends)
 
     def _add_recursive_run_depends(self, packages, full_depends):
+        full_depends.add(self.package.name)
         package_names = packages.keys()
-        for name in [d.name for d in self.run_depends if d.name in package_names and d.name not in full_depends]:
-            full_depends.add(name)
+        for name in [d.name for d in self.package.run_depends if d.name in package_names and d.name not in full_depends]:
             packages[name]._add_recursive_run_depends(packages, full_depends)
 
 
 def topological_order(root_dir, whitelisted=None, blacklisted=None):
+    '''
+    Crawls the filesystem to find packages and uses their dependencies to return a topologically order list.
+
+    :param root_dir: The path to search in, ``str``
+    :param whitelisted: A list of whitelisted package names, ``list``
+    :param blacklisted: A list of blacklisted package names, ``list``
+    :returns: A dict mapping relative paths to ``Package`` objects ``dict``
+    '''
     packages = find_packages(root_dir)
-    decorators = {}
-    for name, package in packages.items():
-        decorators[name] = _PackageDecorator(package)
-    decorators = topological_order_packages(decorators, whitelisted, blacklisted)
-    packages = []
-    for name, decorator in decorators:
-        packages.append([name, decorator.package])
-    return packages
+    return topological_order_packages(packages, whitelisted, blacklisted)
 
 
 def topological_order_packages(packages, whitelisted=None, blacklisted=None):
-    selected = {}
+    '''
+    Topologically orders packages.
+    First returning packages which have message generators and then the rest based on direct build-/buildtool_depends and indirect recursive run_depends.
+
+    :param packages: A dict mapping relative paths to ``Package`` objects ``dict``
+    :param whitelisted: A list of whitelisted package names, ``list``
+    :param blacklisted: A list of blacklisted package names, ``list``
+    :returns: A List of tuples contain the relative path and a ``Package`` object ``list``
+    '''
+    decorators_by_name = {}
     for path, package in packages.items():
         # skip non-whitelisted packages
         if whitelisted and package.name not in whitelisted:
@@ -51,31 +58,32 @@ def topological_order_packages(packages, whitelisted=None, blacklisted=None):
         # skip blacklisted packages
         if blacklisted and package.name in blacklisted:
             continue
-        packages_with_same_name = {k: v for k, v in selected.items() if v.name == package.name}
+        packages_with_same_name = [p for p in decorators_by_name.values() if p.name == package.name]
         if packages_with_same_name:
-            raise RuntimeError('Two packages with the same name "%s" in the workspace:\n- %s\n- %s' % (package.name, selected[path].path, package.path))
-        selected[path] = package
+            path_with_same_name = [p for p, v in packages.items() if v == packages_with_same_name[0]]
+            raise RuntimeError('Two packages with the same name "%s" in the workspace:\n- %s\n- %s' % (package.name, path_with_same_name[0], path))
+        decorators_by_name[package.name] = _PackageDecorator(package, path)
 
     # calculate transitive dependencies
-    for package in selected.values():
-        package.calculate_full_depends(selected)
+    for decorator in decorators_by_name.values():
+        decorator.calculate_full_depends(decorators_by_name)
 
-    return _sort_packages(selected)
+    return _sort_decorated_packages(decorators_by_name)
 
 
-def _sort_packages(packages):
+def _sort_decorated_packages(packages):
     '''
-    First returning packages which have message generators and then the rest based on direct build_depends and indirect recursive run_depends.
+    :param packages: A dict mapping relative paths to ``_PackageDecorator`` objects ``dict``
+    :returns: A List of tuples contain the relative path and a ``Package`` object ``list``
     '''
-
     ordered_packages = []
     while len(packages) > 0:
         # find all packages without build dependencies
         message_generators = []
         non_message_generators = []
-        for name, data in packages.items():
-            if not data.full_depends:
-                if data.message_generator:
+        for name, decorator in packages.items():
+            if not decorator.full_depends:
+                if decorator.message_generator:
                     message_generators.append(name)
                 else:
                     non_message_generators.append(name)
@@ -85,7 +93,7 @@ def _sort_packages(packages):
         elif non_message_generators:
             names = non_message_generators
         else:
-            # in case of a circular dependency pass the list of remaining packages
+            # in case of a circular dependency pass the list of remaining package names
             ordered_packages.append([None, ', '.join(sorted(packages.keys()))])
             break
 
@@ -95,14 +103,10 @@ def _sort_packages(packages):
         # add first candidates to ordered list
         # do not add all candidates since removing the depends from the first might affect the next candidates
         name = names[0]
-        ordered_packages.append([name, packages[name]])
+        ordered_packages.append([packages[name].path, packages[name].package])
         # remove package from further processing
         del packages[name]
         for package in packages.values():
             if name in package.full_depends:
                 package.full_depends.remove(name)
     return ordered_packages
-
-
-def get_message_generators(ordered_packages):
-    return [name for (name, data) in ordered_packages if hasattr(data, 'message_generator') and data.message_generator]
--- test/test_topological_order.py
+++ test/test_topological_order.py
@@ -1,5 +1,7 @@
+from __future__ import print_function
 import os
 import unittest
+import sys
 
 try:
     from catkin_pkg.topological_order import topological_order
@@ -11,6 +13,12 @@ class TopologicalOrderTest(unittest.TestCase):
 
     def test_recursive_run_depends(self):
         test_dir = os.path.join(os.path.dirname(__file__), 'topological_order')
-        packages = topological_order(test_dir)
-        # c must be before b because of the run dependency from a to c
-        self.assertEqual(['a', 'c', 'b'], [name for name, _ in packages])
+
+        packages = topological_order(test_dir, blacklisted=['c'])
+        # d before b because of the run dependency from a to d
+        # a before d only because of alphabetic order, a run depend on d should not influence the order
+        self.assertEqual(['a', 'd', 'b'], [name for name, _ in packages])
+
+        packages = topological_order(test_dir, whitelisted=['a', 'b', 'c'])
+        # c before a because of the run dependency from a to c
+        self.assertEqual(['c', 'a', 'b'], [name for name, _ in packages])
