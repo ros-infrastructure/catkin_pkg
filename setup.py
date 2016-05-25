07a352a57b5395c76f6d5689f2e54e342b28b2dc
Ernesto Corbellini <ecorbellini@ekumenlabs.com>
2016-05-25 14:50:25 -0700

    Converted python package declaration to ament style.

--- setup.py
+++ setup.py
@@ -1,27 +1,38 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+from setuptools import find_packages
 
 import os
 import sys
 source = os.path.join(os.path.dirname(__file__), 'src')
 sys.path.insert(0, source)
 
-from catkin_pkg import __version__
+#from catkin_pkg import __version__
 
 setup(
     name='catkin_pkg',
-    version=__version__,
-    packages=['catkin_pkg'],
-    package_dir={'': 'src'},
-    package_data={'catkin_pkg': ['templates/*.in']},
-    scripts=[
-        'bin/catkin_create_pkg',
-        'bin/catkin_find_pkg',
-        'bin/catkin_generate_changelog',
-        'bin/catkin_tag_changelog',
-        'bin/catkin_test_changelog'
-    ],
+    #version=__version__,
+    version='0.2.10',
+    packages=find_packages(exclude=['test']),
+    #package_dir={'': 'src'},
+    package_data={
+        'catkin_pkg': [
+            'templates/*.in',
+            'bin/catkin_create_pkg',
+            'bin/catkin_find_pkg',
+            'bin/catkin_generate_changelog',
+            'bin/catkin_tag_changelog',
+            'bin/catkin_test_changelog',
+        ],
+    },
+    #scripts=[
+    #    'bin/catkin_create_pkg',
+    #    'bin/catkin_find_pkg',
+    #    'bin/catkin_generate_changelog',
+    #    'bin/catkin_tag_changelog',
+    #    'bin/catkin_test_changelog'
+    #],
     author='Dirk Thomas',
     author_email='dthomas@osrfoundation.org',
     url='http://wiki.ros.org/catkin_pkg',
