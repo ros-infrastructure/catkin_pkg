1b5404f8290fc446a75de77522b28102e1e8ff4c
John Hendy <jw.hendy@gmail.com>
2017-05-16 17:26:04 -0500

    updating template to populate include_directories() with # (#168)
    * updating template to populate include_directories() with #
    
    * revised formatting for include_directories() template per pull request feedback
    
    * Tweak _create_includes_macro to shorten line length on TODO note. Update includes tests to pass with new format
    
    * move result variable closer to where it is used
    
    * simplify

--- src/catkin_pkg/package_templates.py
+++ src/catkin_pkg/package_templates.py
@@ -294,24 +294,23 @@ def _create_targetlib_args(package_template):
 
 
 def _create_include_macro(package_template):
-    result = '# include_directories(include)'
-    includes = []
-    if package_template.catkin_deps:
-        includes.append('${catkin_INCLUDE_DIRS}')
+    includes = ['# include']
+    includes.append(('  ' if package_template.catkin_deps else '# ') + '${catkin_INCLUDE_DIRS}')
     if package_template.boost_comps:
-        includes.append('${Boost_INCLUDE_DIRS}')
+        includes.append('  ${Boost_INCLUDE_DIRS}')
     if package_template.system_deps:
         deplist = []
         for sysdep in package_template.system_deps:
             if not sysdep.startswith('python-'):
                 deplist.append(sysdep)
-                includes.append('${%s_INCLUDE_DIRS}' % sysdep)
         if deplist:
-            result += '\n# TODO: Check names of system library include directories (%s)' % ', '.join(deplist)
+            todo_incl = '# TODO: Check names of system library include directories'
+            includes.append(todo_incl + (' (%s)' % ', '.join(deplist)))
+            includes.extend(['  ${%s_INCLUDE_DIRS}' % sysdep for sysdep in deplist])
+    result = ''
     if includes:
-        result += '\ninclude_directories(\n  %s\n)' % '\n  '.join(includes)
+        result += '\n'.join(includes)
     return result
-
 
 def _create_depend_tag(dep_type,
                        name,
--- test/test_templates.py
+++ test/test_templates.py
@@ -105,27 +105,27 @@ class TemplateTest(unittest.TestCase):
         mock_pack.boost_comps = []
         mock_pack.system_deps = []
         statement = _create_include_macro(mock_pack)
-        self.assertEqual('# include_directories(include)', statement)
+        self.assertEqual('# include\n# ${catkin_INCLUDE_DIRS}', statement)
         mock_pack.catkin_deps = ['roscpp', 'rospy']
         mock_pack.boost_comps = []
         mock_pack.system_deps = []
         statement = _create_include_macro(mock_pack)
-        self.assertEqual('# include_directories(include)\ninclude_directories(\n  ${catkin_INCLUDE_DIRS}\n)', statement)
+        self.assertEqual('# include\n  ${catkin_INCLUDE_DIRS}', statement)
         mock_pack.catkin_deps = ['roscpp']
         mock_pack.boost_comps = ['thread', 'filesystem']
         mock_pack.system_deps = []
         statement = _create_include_macro(mock_pack)
-        self.assertEqual('# include_directories(include)\ninclude_directories(\n  ${catkin_INCLUDE_DIRS}\n  ${Boost_INCLUDE_DIRS}\n)', statement)
+        self.assertEqual('# include\n  ${catkin_INCLUDE_DIRS}\n  ${Boost_INCLUDE_DIRS}', statement)
         mock_pack.catkin_deps = ['roscpp']
         mock_pack.boost_comps = []
         mock_pack.system_deps = ['log4cxx', 'BZip2']
         statement = _create_include_macro(mock_pack)
-        self.assertEqual('# include_directories(include)\n# TODO: Check names of system library include directories (log4cxx, BZip2)\ninclude_directories(\n  ${catkin_INCLUDE_DIRS}\n  ${log4cxx_INCLUDE_DIRS}\n  ${BZip2_INCLUDE_DIRS}\n)', statement)
+        self.assertEqual('# include\n  ${catkin_INCLUDE_DIRS}\n# TODO: Check names of system library include directories (log4cxx, BZip2)\n  ${log4cxx_INCLUDE_DIRS}\n  ${BZip2_INCLUDE_DIRS}', statement)
         mock_pack.catkin_deps = ['roscpp']
         mock_pack.boost_comps = ['thread', 'filesystem']
         mock_pack.system_deps = ['log4cxx', 'BZip2']
         statement = _create_include_macro(mock_pack)
-        self.assertEqual('# include_directories(include)\n# TODO: Check names of system library include directories (log4cxx, BZip2)\ninclude_directories(\n  ${catkin_INCLUDE_DIRS}\n  ${Boost_INCLUDE_DIRS}\n  ${log4cxx_INCLUDE_DIRS}\n  ${BZip2_INCLUDE_DIRS}\n)', statement)
+        self.assertEqual('# include\n  ${catkin_INCLUDE_DIRS}\n  ${Boost_INCLUDE_DIRS}\n# TODO: Check names of system library include directories (log4cxx, BZip2)\n  ${log4cxx_INCLUDE_DIRS}\n  ${BZip2_INCLUDE_DIRS}', statement)
 
     def test_create_package(self):
         maint = self.get_maintainer()
