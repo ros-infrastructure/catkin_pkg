2331a9c647a76950b944a178ffea37e684996fd9
Tamaki Nishino <otamachan@gmail.com>
2018-04-03 06:18:23 +0900

    Add support for file attribution of license tag (#202)

--- test/test_package.py
+++ test/test_package.py
@@ -8,6 +8,7 @@ from catkin_pkg.package import (
     Dependency,
     Export,
     InvalidPackage,
+    License,
     Package,
     Person,
     parse_package,
@@ -67,6 +68,7 @@ class PackageTest(unittest.TestCase):
         self.assertEqual([], pack.authors)
         self.assertEqual([maint], pack.maintainers)
         self.assertEqual(['BSD'], pack.licenses)
+        self.assertEqual([None], [l.file for l in pack.licenses])
         self.assertEqual([], pack.build_depends)
         self.assertEqual([], pack.buildtool_depends)
         self.assertEqual([], pack.run_depends)
@@ -150,7 +152,7 @@ class PackageTest(unittest.TestCase):
 
     def test_init_kwargs_object(self):
         mmain = [self.get_maintainer(), self.get_maintainer()]
-        mlis = ['MIT', 'BSD']
+        mlis = ['MIT', License('BSD', 'LICENSE')]
         mauth = [self.get_maintainer(), self.get_maintainer()]
         murl = [Mock(), Mock()]
         mbuilddep = [Mock(), Mock()]
@@ -182,6 +184,7 @@ class PackageTest(unittest.TestCase):
                        exports=mexp)
         self.assertEqual(mmain, pack.maintainers)
         self.assertEqual(mlis, pack.licenses)
+        self.assertEqual([None, 'LICENSE'], [l.file for l in pack.licenses])
         self.assertEqual(murl, pack.urls)
         self.assertEqual(mauth, pack.authors)
         self.assertEqual(mbuilddep, pack.build_depends)
