ee7fc82a0c40f7194abf5422707069fa0dda846b
Nick Medveditskov <Nickolaim@users.noreply.github.com>
2018-04-04 10:08:40 -0700

    InvalidPackage with package path (#209)
    * `parse_package_string()` raises `InvalidPackage` exception with the package name in the message.
    This work is needed for [https://github.com/ros-infrastructure/rosdep/issues/598](https://github.com/ros-infrastructure/rosdep/issues/598)
    
    * Add filename to the InvalidPackage exception coming from `Package.validate()`
    
    * `InvalidPackage` has `msg` and`package_path` (#1)
    
    `InvalidPackage` exception has `msg`, `package_path` and defines `__str__()`
    
    Note that `parse_package_string()` catches `InvalidPackage` exception and sets
    the filename; it is needed because the functions that called in
    `_parse_package_string()` raise InvalidPackage exception without filename,
    e.g. `_get_node()`
    
    * Minor code review fixes
    - Use double quotes for string literals with single quotes inside
    - Use forward slash instead of backward slash in file path
    
    * Pass filename to functions that raise InvalidPackage
    
    Remove not used `_get_optional_node_value()`
    Remove try/except block that was adding filename to InvalidPackage
    exception
    
    * nipick: improve error message

--- test/test_packages.py
+++ test/test_packages.py
@@ -1,21 +1,38 @@
 import os
 
+from catkin_pkg.package import InvalidPackage
+
+from catkin_pkg.packages import find_packages
 from catkin_pkg.packages import find_package_paths
 from catkin_pkg.packages import find_packages_allowing_duplicates
 
 from .util import in_temporary_directory
 
+def _create_pkg_in_dir(path, version='0.1.0'):
+    path = os.path.abspath(path)
+    os.makedirs(path)
 
-def _create_pkg_in_dir(path):
-    os.mkdir(path)
-    open(os.path.join(path, 'package.xml'), 'a').close()
+    template = """\
+<?xml version="1.0"?>
+<package>
+  <name>{0}</name>
+  <version>{1}</version>
+  <description>Package {0}</description>
+  <license>BSD</license>
+
+  <maintainer email="foo@bar.com">Foo Bar</maintainer>
+</package>
+""".format(path.split('/')[-1], version)
+
+    with open(os.path.join(path, 'package.xml'), 'w+') as f:
+        f.write(template)
 
 @in_temporary_directory
 def test_package_paths_with_hidden_directories():
-    _create_pkg_in_dir('.test1');
-    _create_pkg_in_dir('.test2');
-    _create_pkg_in_dir('test3'); # not hidden
-    _create_pkg_in_dir('.test4');
+    _create_pkg_in_dir('.test1')
+    _create_pkg_in_dir('.test2')
+    _create_pkg_in_dir('test3')  # not hidden
+    _create_pkg_in_dir('.test4')
 
     res = find_package_paths('.')
     assert res == ['test3']
@@ -25,3 +42,16 @@ def test_find_packages_allowing_duplicates_with_no_packages():
     res = find_packages_allowing_duplicates('.')
     assert isinstance(res, dict)
     assert not res
+
+@in_temporary_directory
+def test_find_packages_invalid_version():
+    version = ':{version}'
+    path = 'src/foo'
+    _create_pkg_in_dir(path, version)
+    try:
+        find_packages(path.split('/')[0])
+        assert False, 'Must raise'
+    except InvalidPackage as e:
+        exception_message = str(e)
+        assert version in exception_message
+        assert path in exception_message
