e91b17915abcd5bd8edfb548896404563de13d5b
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2018-05-31 10:47:29 -0700

    move catkin_prepare_release and dependencies from catkin (#216)
    * move catkin_prepare_release and dependencies from catkin
    
    * fix style warnings, Python 3 compatibility
    
    * move corresponding tests

---
+++ test/test_terminal_color.py
@@ -0,0 +1,28 @@
+import unittest
+
+from catkin_pkg.terminal_color import ansi, enable_ANSI_colors, fmt, sanitize
+
+try:
+    char = unichr
+except NameError:
+    char = chr
+
+
+class TerminalColorTest(unittest.TestCase):
+
+    def test_terminal_colors(self):
+        # since other test might disable ansi colors
+        # we need to ensure it is enabled
+        enable_ANSI_colors()
+        assert ansi('reset') != ''
+        test = '@_This is underlined@|'
+        rslt = '\033[4mThis is underlined\033[0m\033[0m'
+        assert fmt(test) == rslt
+        test = 'This has bad stuff @! @/ @_ @| OK!'
+        test = sanitize(test)
+        rslt = 'This has bad stuff @! @/ @_ @| OK!\033[0m'
+        assert fmt(test) == rslt
+        test = char(2018)
+        test = sanitize(test)
+        rslt = char(2018)
+        assert test == rslt
