9fb27c42b5677f7590bdf592ea4ea615d495ac59
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2018-07-26 14:50:00 -0700

    use version number on -modules dependency

--- setup.py
+++ setup.py
@@ -6,7 +6,10 @@ from setuptools import setup
 
 kwargs = {
     'name': 'catkin_pkg',
-    'version': '0.4.6',  # same version as in src/catkin_pkg/__init__.py
+    # same version as in:
+    # - src/catkin_pkg/__init__.py
+    # - stdeb.cfg
+    'version': '0.4.6',
     'packages': ['catkin_pkg', 'catkin_pkg.cli'],
     'package_dir': {'': 'src'},
     'package_data': {'catkin_pkg': ['templates/*.in']},
