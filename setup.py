01477dc945eee98f37ef2160302fdfcdac79c01e
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2020-01-25 13:09:42 -0800

    0.4.16

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ kwargs = {
     # same version as in:
     # - src/catkin_pkg/__init__.py
     # - stdeb.cfg
-    'version': '0.4.15',
+    'version': '0.4.16',
     'packages': ['catkin_pkg', 'catkin_pkg.cli'],
     'package_dir': {'': 'src'},
     'package_data': {'catkin_pkg': ['templates/*.in']},
