7e45b7141153bf8e01e327220bace4b890a0b47e
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2020-04-15 20:52:48 +0200

    use parse actions to evaluate conditions (#283)
    * [fix] handle multiple or in condition
    
    Inspired by https://github.com/pyparsing/pyparsing/blob/master/examples/simpleBool.py
    With the use of ParseActions, the result are specific classes in which
    better use is made of the results of the operatorPrecedence.
    
    * (travis) fix warnings
    
    * revert unrelated change to .travis.yml
    
    * move evaluate_condition() function to the original location to minimize diff
    
    * move new classes to the end in order of usage
    
    * prefix new classes with an underscore to not make them public API
    
    Co-authored-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>

--- src/catkin_pkg/condition.py
+++ src/catkin_pkg/condition.py
@@ -14,7 +14,7 @@
 
 import operator
 
-import pyparsing
+import pyparsing as pp
 
 
 def evaluate_condition(condition, context):
@@ -23,10 +23,10 @@ def evaluate_condition(condition, context):
     expr = _get_condition_expression()
     try:
         parse_results = expr.parseString(condition, parseAll=True)
-    except pyparsing.ParseException as e:
+    except pp.ParseException as e:
         raise ValueError(
             "condition '%s' failed to parse: %s" % (condition, e))
-    return _evaluate(parse_results.asList()[0], context)
+    return parse_results[0](context)
 
 
 _condition_expression = None
@@ -35,43 +35,29 @@ _condition_expression = None
 def _get_condition_expression():
     global _condition_expression
     if not _condition_expression:
-        pp = pyparsing
         operator = pp.Regex('==|!=|>=|>|<=|<').setName('operator')
-        identifier = pp.Word('$', pp.alphanums + '_', min=2)
-        value = pp.Word(pp.alphanums + '_-')
+        operator.setParseAction(_Operator)
+
+        identifier = pp.Word('$', pp.alphanums + '_', min=2).setName('identifier')
+        identifier.setParseAction(_Identifier)
+
+        value = pp.Word(pp.alphanums + '_-').setName('value')
+        value.setParseAction(_Value)
+
         comparison_term = identifier | value
-        condition = pp.Group(comparison_term + operator + comparison_term)
+
+        condition = pp.Group(comparison_term + operator + comparison_term).setName('condition')
+        condition.setParseAction(_Condition)
+
         _condition_expression = pp.infixNotation(
             condition, [
-                ('and', 2, pp.opAssoc.LEFT, ),
-                ('or', 2, pp.opAssoc.LEFT, ),
+                ('and', 2, pp.opAssoc.LEFT, _And),
+                ('or', 2, pp.opAssoc.LEFT, _Or),
             ])
     return _condition_expression
 
 
-def _evaluate(parse_results, context):
-    if not isinstance(parse_results, list):
-        if parse_results.startswith('$'):
-            # get variable from context
-            return str(context.get(parse_results[1:], ''))
-        # return literal value
-        return parse_results
-
-    # recursion
-    assert len(parse_results) >= 3 and len(parse_results) % 2 == 1
-
-    # handle logical operators
-    if parse_results[1] == 'and':
-        return _evaluate(parse_results[0], context) and \
-            _evaluate(parse_results[2] if len(parse_results) == 3
-                      else parse_results[2:], context)
-    if parse_results[1] == 'or':
-        return _evaluate(parse_results[0], context) or \
-            _evaluate(parse_results[2] if len(parse_results) == 3
-                      else parse_results[2:], context)
-
-    # handle comparison operators
-    assert len(parse_results) == 3
+class _Operator:
     operators = {
         '==': operator.eq,
         '!=': operator.ne,
@@ -80,7 +66,82 @@ def _evaluate(parse_results, context):
         '>=': operator.ge,
         '>': operator.gt,
     }
-    assert parse_results[1] in operators.keys()
-    return operators[parse_results[1]](
-        _evaluate(parse_results[0], context),
-        _evaluate(parse_results[2], context))
+
+    def __init__(self, t):
+        self.value = t[0]
+
+    def __call__(self, arg1, arg2, context):
+        assert self.value in self.operators
+        return self.operators[self.value](arg1(context), arg2(context))
+
+    def __str__(self):
+        return self.value
+
+    __repr__ = __str__
+
+
+class _Identifier:
+
+    def __init__(self, t):
+        self.value = t[0]
+
+    def __call__(self, context):
+        return str(context.get(self.value[1:], ''))
+
+    def __str__(self):
+        return self.value
+
+    __repr__ = __str__
+
+
+class _Value:
+
+    def __init__(self, t):
+        self.value = t[0]
+
+    def __call__(self, context):
+        return self.value
+
+    def __str__(self):
+        return self.value
+
+    __repr__ = __str__
+
+
+class _Condition:
+
+    def __init__(self, t):
+        self.value = t[0]
+
+    def __call__(self, context):
+        return self.value[1](self.value[0], self.value[2], context)
+
+    def __str__(self):
+        return ' '.join(map(str, self.value))
+
+    __repr__ = __str__
+
+
+class _BinOp:
+
+    def __init__(self, t):
+        self.args = t[0][0::2]
+
+    def __call__(self, context):
+        return self.evalop(a(context) for a in self.args)
+
+    def __str__(self):
+        sep = ' %s  ' % self.reprsymbol
+        return '(' + sep.join(map(str, self.args)) + ')'
+
+    __repr__ = __str__
+
+
+class _And(_BinOp):
+    reprsymbol = 'and'
+    evalop = all
+
+
+class _Or(_BinOp):
+    reprsymbol = 'or'
+    evalop = any
