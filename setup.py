ros-infrastructure/catkin_pkg

commit 698cae13edca1a25f4fa2261d781fc7f56f14e0e

Fix crash with pyparsing 3 (#282)
* Fix crash with pyparsing 3

pyparsing.operatorPrecedence was deprecated in pyparsing 1.5.7 and removed in pyparsing 3

https://github.com/pyparsing/pyparsing/blob/master/CHANGES
```
colcon build --packages-up-to rmw_cyclonedds_cpp
[8.139s] ERROR:colcon.colcon_core.package_identification:Exception in package identification extension 'ros.bazel' in '/opt/ros/master/src/osrf/osrf_pycommon': module 'pyparsing' has no attribute 'operatorPrecedence'
Traceback (most recent call last):
  File "/Users/dan/Documents/colcon_ws/src/colcon-core/colcon_core/package_identification/__init__.py", line 143, in _identify
    retval = extension.identify(_reused_descriptor_instance)
  File "/Users/dan/Documents/colcon_ws/src/colcon-ros-bazel/colcon_ros_bazel/package_identification/ros_bazel.py", line 39, in identify
    ros_extension.identify(tmp_desc)
  File "/Users/dan/Documents/colcon_ws/src/colcon-ros/colcon_ros/package_identification/ros.py", line 59, in identify
    pkg, build_type = get_package_with_build_type(str(desc.path))
  File "/Users/dan/Documents/colcon_ws/src/colcon-ros/colcon_ros/package_identification/ros.py", line 163, in get_package_with_build_type
    pkg = _get_package(path)
  File "/Users/dan/Documents/colcon_ws/src/colcon-ros/colcon_ros/package_identification/ros.py", line 195, in _get_package
    pkg.evaluate_conditions(os.environ)
  File "/usr/local/Cellar/python/HEAD-29356e0/Frameworks/Python.framework/Versions/3.9/lib/python3.9/site-packages/catkin_pkg/package.py", line 212, in evaluate_conditions
    conditional.evaluate_condition(context)
  File "/usr/local/Cellar/python/HEAD-29356e0/Frameworks/Python.framework/Versions/3.9/lib/python3.9/site-packages/catkin_pkg/package.py", line 369, in evaluate_condition
    self.evaluated_condition = evaluate_condition(self.condition, context)

setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 install_requires = [
     'docutils',
     'python-dateutil',
-    'pyparsing',
+    'pyparsing>=1.5.7',
 ]
 
 # argparse is part of the standard library since Python 2.7

src/catkin_pkg/condition.py
@@ -41,7 +41,7 @@ def _get_condition_expression():
         value = pp.Word(pp.alphanums + '_-')
         comparison_term = identifier | value
         condition = pp.Group(comparison_term + operator + comparison_term)
-        _condition_expression = pp.operatorPrecedence(
+        _condition_expression = pp.infixNotation(
             condition, [
                 ('and', 2, pp.opAssoc.LEFT, ),
                 ('or', 2, pp.opAssoc.LEFT, ),

commit 96140d4d4c60731398a452b21ba777db508b07b7

Restore compatibility with pyparsing < 1.5.7
operatorPrecedence renamed to infixNotation in pyparsing 1.5.7. RHEL 7
currently has pyparsing 1.5.6.

Compatibility was regressed by 698cae1

setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 install_requires = [
     'docutils',
     'python-dateutil',
-    'pyparsing>=1.5.7',
+    'pyparsing',
 ]
 
 # argparse is part of the standard library since Python 2.7

src/catkin_pkg/condition.py
@@ -16,6 +16,12 @@ import operator
 
 import pyparsing as pp
 
+# operatorPrecedence renamed to infixNotation in 1.5.7
+try:
+    from pyparsing import infixNotation as infixNotation
+except ImportError:
+    from pyparsing import operatorPrecedence as infixNotation
+
 
 def evaluate_condition(condition, context):
     if condition is None:
@@ -49,7 +55,7 @@ def _get_condition_expression():
         condition = pp.Group(comparison_term + operator + comparison_term).setName('condition')
         condition.setParseAction(_Condition)
 
-        _condition_expression = pp.infixNotation(
+        _condition_expression = infixNotation(
             condition, [
                 ('and', 2, pp.opAssoc.LEFT, _And),
                 ('or', 2, pp.opAssoc.LEFT, _Or),