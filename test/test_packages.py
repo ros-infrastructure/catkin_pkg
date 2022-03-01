423eae7208f7ddfaf71f30d01be13797daaa55f8
Steven! Ragnarök <steven@nuclearsandwich.com>
2022-02-28 21:08:22 -0800

    Add tests for using ignore files with and without custom ignore arguments.

--- test/test_packages.py
+++ test/test_packages.py
@@ -8,6 +8,8 @@ from catkin_pkg.packages import find_packages_allowing_duplicates
 from .util import in_temporary_directory
 
 
+test_data_dir = os.path.join(os.path.dirname(__file__), 'data', 'ignored_packages')
+
 def _create_pkg_in_dir(path, version='0.1.0'):
     path = os.path.abspath(path)
     os.makedirs(path)
@@ -58,3 +60,15 @@ def test_find_packages_invalid_version():
         exception_message = str(e)
         assert version in exception_message
         assert path in exception_message
+
+
+def test_find_no_ignored_packages():
+    result = find_packages(test_data_dir)
+    assert 'catkin_ignore'  not in result
+    assert 'custom_ignore'  in result
+
+
+def test_find_no_ignored_packages_with_custom_ignore():
+    custom_result = find_packages(test_data_dir, ignore_markers={'CUSTOM_IGNORE'})
+    assert 'custom_ignore'  not in custom_result
+    assert 'catkin_ignore'  in custom_result
