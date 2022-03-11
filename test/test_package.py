4bba52038293df1d6e7aec86fbea542ed0ac0a3b
Steven! Ragnarök <steven@nuclearsandwich.com>
2022-03-11 15:56:59 -0800

    Escape newlines at the start and end of multiline string.
    We don't want these newlines in the expected result.

--- test/test_package.py
+++ test/test_package.py
@@ -385,10 +385,10 @@ class PackageTest(unittest.TestCase):
         filename = os.path.join(test_data_dir, 'xhtml_description.xml')
         package = parse_package(filename)
         assert package.description
-        assert package.plaintext_description() == """
+        assert package.plaintext_description() == """\
 A package with an XHTML description.
 
-This package contains several xhtml tags which are, according to REP-149, meant to be handled properly but "XML tags and multiple whitespaces" may be stripped in some situations.
+This package contains several xhtml tags which are, according to REP-149, meant to be handled properly but "XML tags and multiple whitespaces" may be stripped in some situations.\
         """
 
     def test_parse_package_string(self):
