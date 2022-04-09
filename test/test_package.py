49293a107a898d6f254bc7797bfd028566dbaa1f
Steven! Ragnarök <steven@nuclearsandwich.com>
2022-04-08 17:46:59 -0700

    Fix actual discrepancies between the test data and the expected result.

--- test/test_package.py
+++ test/test_package.py
@@ -388,7 +388,7 @@ class PackageTest(unittest.TestCase):
         assert package.get_plaintext_description() == """\
 A package with an XHTML description.
 
-This package contains several xhtml tags which are, according to REP-149, meant to be handled properly but "XML tags and multiple whitespaces" may be stripped in some situations. Ahother sentence in this quasi-paragraph will continue to appear on the same plaintext line becuase there was no <br/> tag to indicate a newline should appear in the output text.
+This package contains several xhtml tags which are, according to REP-149, meant to be properly handled but "XML tags and multiple whitespaces" may be stripped in some situations. Another sentence in this quasi-paragraph will continue to appear on the same plaintext line because there was no <br/> tag to indicate a newline should appear in the output text.
 This text should appear on a subsequent line.\
 """
 
