ros-infrastructure/catkin_pkg

commit e8d6fc4639f6944cefa0240a8f5dad5ba44428cb

update catkin_prepare_release to support setup.py files (#331)
* update catkin_prepare_release to support setup.py files

Signed-off-by: William Woodall <william@osrfoundation.org>

* address flake8 issues

Signed-off-by: William Woodall <william@osrfoundation.org>

* Update src/catkin_pkg/package_version.py

Co-authored-by: Jacob Perron <jacob@openrobotics.org>

* fixup a test case

Signed-off-by: William Woodall <william@osrfoundation.org>

* limit updates to ament_python packages

Signed-off-by: William Woodall <william@osrfoundation.org>

* Use Package.get_build_type method instead of writing a new one.

Co-authored-by: Jacob Perron <jacob@openrobotics.org>
Co-authored-by: Steven! Ragnarök <steven@nuclearsandwich.com>

src/catkin_pkg/cli/prepare_release.py
@@ -133,6 +133,7 @@ def check_clean_working_copy(base_path, vcs_type):
 def commit_files(base_path, vcs_type, packages, packages_with_changelogs, message, dry_run=False):
     cmd = [_find_executable(vcs_type), 'commit', '-m', message]
     cmd += [os.path.join(p, PACKAGE_MANIFEST_FILENAME) for p in packages.keys()]
+    cmd += [s for s in [os.path.join(p, 'setup.py') for p in packages.keys()] if os.path.exists(s)]
     cmd += [path for path, _, _ in packages_with_changelogs.values()]
     if not dry_run:
         try:
@@ -266,17 +267,18 @@ def _main():
     # complain about packages with upper case character since they won't be releasable with bloom
     unsupported_pkg_names = []
     invalid_pkg_names = []
+    valid_build_types = ['catkin', 'ament_cmake', 'ament_python']
     for package in packages.values():
-        build_types = [export.content for export in package.exports if export.tagname == 'build_type']
-        build_type = build_types[0] if build_types else 'catkin'
-        if build_type not in ('catkin', 'ament_cmake'):
+        build_type = package.get_build_type()
+        if build_type not in valid_build_types:
             unsupported_pkg_names.append(package.name)
         if package.name != package.name.lower():
             invalid_pkg_names.append(package.name)
     if unsupported_pkg_names:
         print(
             fmt(
-                "@{yf}Warning: the following package are not of build_type catkin or ament_cmake and may require manual steps to release': %s" %
+                "@{yf}Warning: the following package are not of build_type %s and may require manual steps to release': %s" %
+                str(valid_build_types),
                 ', '.join([('@{boldon}%s@{boldoff}' % p) for p in sorted(unsupported_pkg_names)])
             ), file=sys.stderr)
         if not args.non_interactive and not prompt_continue('Continue anyway', default=False):
@@ -304,6 +306,10 @@ def _main():
                 raise RuntimeError(fmt(
                     "@{rf}Invalid metapackage at path '@{boldon}%s@{boldoff}':\n  %s\n\nSee requirements for metapackages: %s" %
                     (os.path.abspath(pkg_path), str(e), metapackage.DEFINITION_URL)))
+        # verify that the setup.py files don't have modifications pending
+        setup_py_path = os.path.join(pkg_path, 'setup.py')
+        if os.path.exists(setup_py_path) and has_changes(base_path, setup_py_path, vcs_type):
+            local_modifications.append(setup_py_path)
 
     # fetch current version and verify that all packages have same version number
     old_version = verify_equal_package_versions(packages.values())
@@ -380,7 +386,7 @@ def _main():
         (new_version, ', '.join([('@{boldon}%s@{boldoff}' % p) for p in sorted(missing_changelogs_but_forthcoming.keys())]))))
 
     # bump version number
-    update_versions(packages.keys(), new_version)
+    update_versions(packages, new_version)
     print(fmt("@{gf}Bump version@{reset} of all packages from '@{bf}%s@{reset}' to '@{bf}@{boldon}%s@{boldoff}@{reset}'" % (old_version, new_version)))
 
     pushed = None

src/catkin_pkg/package_version.py
@@ -84,6 +84,28 @@ def _replace_version(package_str, new_version):
     return new_package_str
 
 
+def _replace_setup_py_version(setup_py_str, new_version):
+    """
+    Replace the version tag in contents if there is only one instance and it is using a literal as the version.
+
+    :param str package_str: contents of setup.py
+    :param str new_version: new version number
+    :returns: new setup.py string
+    :rtype: str
+    :raises RuntimeError:
+    """
+    # try to replace contents
+    new_setup_py_str, number_of_subs = re.subn(
+        r'version=([\'"])\d+\.\d+\.\d+([\'"]),',
+        r'version=\g<1>%s\g<2>,' % new_version,
+        setup_py_str)
+    if number_of_subs == 0:
+        raise RuntimeError("Failed to find a normal version statement, e.g.: version='1.2.3',")
+    if number_of_subs != 1:
+        raise RuntimeError('Illegal number of version statements: %s' % (number_of_subs))
+    return new_setup_py_str
+
+
 def _check_for_version_comment(package_str, new_version):
     """
     Check if a comment is present behind the version tag and return it.
@@ -101,16 +123,17 @@ def _check_for_version_comment(package_str, new_version):
     return comment
 
 
-def update_versions(paths, new_version):
+def update_versions(packages, new_version):
     """
-    Bulk replace of version: searches for package.xml files directly in given folders and replaces version tag within.
+    Bulk replace of version: searches for package.xml and setup.py files directly in given folders and replaces version tag within.
 
-    :param list paths: folder names
+    :param dict packages: dict from folder names to package xml objects in those folders
     :param str new_version: version string "int.int.int"
     :raises RuntimeError: if any one package.xml cannot be updated
     """
     files = {}
-    for path in paths:
+    for path, package_obj in packages.items():
+        # Update any package.xml files.
         package_path = os.path.join(path, 'package.xml')
         with open(package_path, 'r') as f:
             package_str = f.read()
@@ -122,6 +145,20 @@ def update_versions(paths, new_version):
         except RuntimeError as rue:
             raise RuntimeError('Could not bump version number in file %s: %s' % (package_path, str(rue)))
         files[package_path] = new_package_str
+        # Update any setup.py files.
+        setup_py_path = os.path.join(path, 'setup.py')
+        if os.path.exists(setup_py_path):
+            # Only update setup.py for ament_python packages.
+            build_type = package_obj.get_build_type()
+            if build_type == 'ament_python':
+                with open(setup_py_path, 'r') as f:
+                    setup_py_str = f.read()
+                try:
+                    new_setup_py_str = _replace_setup_py_version(setup_py_str, new_version)
+                except RuntimeError as exc:
+                    raise RuntimeError('Could not bump version number in file %s: %s' % (setup_py_path, str(exc)))
+                files[setup_py_path] = new_setup_py_str
+
     # if all replacements successful, write back modified package.xml
     for package_path, new_package_str in files.items():
         with open(package_path, 'w') as f:
@@ -140,7 +177,7 @@ def get_forthcoming_label(rst):
             if len(section.children) > 0 and isinstance(section.children[0], docutils.nodes.title):
                 title = section.children[0]
         if title and len(title.children) > 0 and isinstance(title.children[0], docutils.nodes.Text):
-            title_text = title.children[0].rawsource
+            title_text = title.children[0].astext()
             if FORTHCOMING_LABEL.lower() in title_text.lower():
                 if forthcoming_label:
                     raise RuntimeError('Found multiple forthcoming sections')

test/test_package_version.py
@@ -4,6 +4,7 @@ import shutil
 import tempfile
 import unittest
 
+from catkin_pkg.package_version import _replace_setup_py_version
 from catkin_pkg.package_version import _replace_version
 from catkin_pkg.package_version import bump_version
 from catkin_pkg.package_version import update_changelog_sections
@@ -45,6 +46,34 @@ class PackageVersionTest(unittest.TestCase):
         self.assertRaises(RuntimeError, _replace_version, '<package></package>', '0.1.1')
         self.assertRaises(RuntimeError, _replace_version, '<package><version>0.1.1</version><version>0.1.1</version></package>', '0.1.1')
 
+    def test_replace_setup_py_version(self):
+        self.assertEqual(
+            'version="1.0.0",',
+            _replace_setup_py_version('version="0.0.1",', '1.0.0'))
+        self.assertEqual(
+            "version='1.0.0',",
+            _replace_setup_py_version("version='0.0.1',", '1.0.0'))
+        self.assertRaises(
+            RuntimeError,
+            _replace_setup_py_version,
+            '',
+            '1.0.0')
+        self.assertRaises(
+            RuntimeError,
+            _replace_setup_py_version,
+            "version='0.1'",
+            '1.0.0')
+        self.assertRaises(
+            RuntimeError,
+            _replace_setup_py_version,
+            'version=something,',
+            '1.0.0')
+        self.assertRaises(
+            RuntimeError,
+            _replace_setup_py_version,
+            'version="0.0.1",version="0.0.1",',
+            '1.0.0')
+
     def test_update_versions(self):
         try:
             root_dir = tempfile.mkdtemp()
@@ -55,7 +84,7 @@ class PackageVersionTest(unittest.TestCase):
             with open(os.path.join(sub_dir, 'package.xml'), 'w') as fhand:
                 fhand.write('<package><version>1.5.4</version></package>')
 
-            update_versions([root_dir, sub_dir], '7.6.5')
+            update_versions({root_dir: Mock(), sub_dir: Mock()}, '7.6.5')
 
             with open(os.path.join(root_dir, 'package.xml'), 'r') as fhand:
                 contents = fhand.read()

commit 239a5f6f803d460687b307c331f6a4626c8106b4

Pass all string format arguments as a tuple
Fixes: e8d6fc4639f6944cefa0240a8f5dad5ba44428cb

src/catkin_pkg/cli/prepare_release.py
@@ -278,8 +278,7 @@ def _main():
         print(
             fmt(
                 "@{yf}Warning: the following package are not of build_type %s and may require manual steps to release': %s" %
-                str(valid_build_types),
-                ', '.join([('@{boldon}%s@{boldoff}' % p) for p in sorted(unsupported_pkg_names)])
+                (str(valid_build_types), ', '.join([('@{boldon}%s@{boldoff}' % p) for p in sorted(unsupported_pkg_names)]))
             ), file=sys.stderr)
         if not args.non_interactive and not prompt_continue('Continue anyway', default=False):
             raise RuntimeError(fmt('@{rf}Aborted release, verify that unsupported packages are ready to be released or release manually.'))